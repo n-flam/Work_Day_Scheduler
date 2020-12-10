82e3e1038a13e5e78bd8fe51f30c100c613a2ba6
Nadja Flaming <nadja.flam@gmx.net>
2020-12-10 17:40:30 +0800

    canges in JS and HTML

--- Assets/app.js
+++ Assets/app.js
@@ -1,90 +1,159 @@
-// load time into the dom
 
+$(document).ready(function() {
 
-setInterval(function(){
-    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
-
-}, 1000);
-
-
-// Declatring var/const
-const planWorkday = [
-    {time : "9am", event: "" },
-    {time : "10am", event: "" },
-    {time : "11am", event: "" },
-    {time : "12pm", event: "" },
-    {time : "1m", event: "" },
-    {time : "2pm", event: "" },
-    {time : "3pm", event: "" },
-    {time : "4pm", event: "" },
-    {time : "5pm", event: "" },
-];
-
-
-// set up table
-// shouldn't create the timetable via raw html
-planWorkday.forEach(function(timeBlock, index) {
-	var timeLabel = timeBlock.time;
-	var blockColor = colorRow(timeLabel);
-	var row =
-		'<div class="time-block" id="' +
-		index +
-		'"><div class="row no-gutters input-group"><div class="col-sm col-lg-1 input-group-prepend hour justify-content-sm-end pr-3 pt-3">' +
-		timeLabel +
-		'</div><textarea class="form-control ' +
-		blockColor +
-		'">' +
-		timeBlock.event +
-		'</textarea><div class="col-sm col-lg-1 input-group-append"><button class="saveBtn btn-block" type="submit"><i class="fas fa-save"></i></button></div></div></div>';
-
-	/* Adding rows to container div */
-	$(".container").append(row);
-});
-
-
-
-
-
-
-// for loop 
-for (let index = 0; index < times.length; index++) {
-    const time = times[index];
-    // in each loop we create a time row
-
-    // each row would have the time
-    // textarea
-    // button
-
-    // attacj event listner
-    // Todo: const button;
-
-    $(button).on('click', function(event){
-        event.preventDeault();
-
-        // identify which row
-
-        $(event.target).parent()
-
-
-        // row
-        // Todo: const row;
-        // const time = row.firstChild().text()
-
-        // i want to get the value of textarea
-        // data = row.secondChild().firstChild().val()
-        // TODO: 
-
-        // FIXME: 
-        localStorage.setItem('data-row-' + time, data)
-
+ 
+    // variables and array
+    let hours = ["hour8", "hour9", "hour10", "hour11","hour12", "hour13", "hour14","hour15", "hour16", "hour17"];
+    let toDo = ["#to-do8", "#to-do9", "#to-do10","#to-d11", "#to-do12", "#to-do13","#to-do14", "#to-do15", "#to-do16", "#to-do17"];
+    let times = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
+  
+    // moment.js date/time request
+    var update = function() {
+      document.getElementById("currentDay")
+      .innerHTML = moment().format('dddd, MMMM Do YYYY, h:mm:ss a');
+    }
+    setInterval(update, 1000);
+   
+    // toDo:
+    // <div class="container">             
+    //     <div class="row" id="hour-eight">
+    //         <div class="col-sm-1" id="hour8">
+    //           8:00 AM
+    //         </div>
+    //             <input type="text" class="form-control col-sm-10" id="to-do8">
+    //             <button type="button" class="btn btn-primary savebtn"><i class="far fa-calendar-alt"></i></button>
+    //     </div>
+  
+  
+  
+    // Get save button to add textarea text to local storage
+    $(".savebtn").on("click", function() {
+      
+      let textarea = $(this).siblings(".form-control").val();
+      let hour = $(this).siblings().attr("id");
+  
+      localStorage.setItem(hour, textarea);
     });
-
-
-    // create a data attr on the row
-    // data-row-time 
-    // so we can identify each row
-    // and create a unique key in local storage
-
-
-
-}
+  
+   // Setting textarea area for each hour to local storage
+    for (let i = 0; i<hours.length; i++) {
+      $(toDo[i]).val(localStorage.getItem(hours[i]));
+    };
+   
+    // simplify code for color coding 
+    // for (let x = 0; i < times.length; x++) {
+      
+      // };
+      
+      // Color coding present, past, and future hours
+    if ((moment().format("H")) > 8 ) {
+      $(hour8).css("background-color", "#b6b6b6");
+    } else if ((moment().format("H")) < 8) {
+      $(hour8).css("background-color", "#77dd77");
+    } else if ((moment().format("H")) == 8) {
+      $(hour8).css("background-color", "#ff6961");
+    } else {
+      alert('Error');
+    }
+  
+  
+    if ((moment().format("H")) > 9) {
+      $(hour9).css("background-color", "#b6b6b6");
+    } else if ((moment().format("H")) < 9) {
+      $(hour9).css("background-color", "#77dd77");
+    } else if ((moment().format("H")) == 9) {
+      $(hour9).css("background-color", "#ff6961");
+    } else {
+      alert('Error');
+    }
+  
+  
+    if ((moment().format("H")) > 10) {
+      $(hour10).css("background-color", "#b6b6b6");
+    } else if ((moment().format("H")) < 10) {
+      $(hour10).css("background-color", "#77dd77");
+    } else if ((moment().format("H")) == 10) {
+      $(hour10).css("background-color", "#ff6961");
+    } else {
+      alert('Error');
+    }
+  
+  
+    if ((moment().format("H")) > 11) {
+      $(hour11).css("background-color", "#b6b6b6");
+    } else if ((moment().format("H")) < 11) {
+      $(hour11).css("background-color", "#77dd77");
+    } else if ((moment().format("H")) == 11) {
+      $(hour11).css("background-color", "#ff6961");
+    } else {
+      alert('Error');
+    }
+  
+  
+    if ((moment().format("H")) > 12) {
+      $(hour12).css("background-color", "#b6b6b6");
+    } else if ((moment().format("H")) < 12) {
+      $(hour12).css("background-color", "#77dd77");
+    } else if ((moment().format("H")) == 12) {
+      $(hour12).css("background-color", "#ff6961");
+    } else {
+      alert('Error');
+    }
+  
+  
+    if ((moment().format("H")) > 13) {
+      $(hour13).css("background-color", "#b6b6b6");
+    } else if ((moment().format("H")) < 13) {
+      $(hour13).css("background-color", "#77dd77");
+    } else if ((moment().format("H")) == 13) {
+      $(hour13).css("background-color", "#ff6961");
+    } else {
+      alert('Error');
+    }
+  
+  
+    if ((moment().format("H")) > 14) {
+      $(hour14).css("background-color", "#b6b6b6");
+    } else if ((moment().format("H")) < 14) {
+      $(hour14).css("background-color", "#77dd77");
+    } else if ((moment().format("H")) == 14) {
+      $(hour14).css("background-color", "#ff6961");
+    } else {
+      alert('Error');
+    }
+  
+  
+    if ((moment().format("H")) > 15) {
+      $(hour15).css("background-color", "#b6b6b6");
+    } else if ((moment().format("H")) < 15) {
+      $(hour15).css("background-color", "#77dd77");
+    } else if ((moment().format("H")) == 15) {
+      $(hour15).css("background-color", "#ff6961");
+    } else {
+      alert('Error');
+    }
+  
+  
+    if ((moment().format("H")) > 16) {
+      $(hour16).css("background-color", "#b6b6b6");
+    } else if ((moment().format("H")) < 16) {
+      $(hour16).css("background-color", "#77dd77");
+    } else if ((moment().format("H")) == 16) {
+      $(hour16).css("background-color", "#ff6961");
+    } else {
+      alert('Error');
+    }
+  
+  
+    if ((moment().format("H")) > 17) {
+      $(hour17).css("background-color", "#b6b6b6");
+    } else if ((moment().format("H")) < 17) {
+      $(hour17).css("background-color", "#77dd77");
+    } else if ((moment().format("H")) == 17) {
+      $(hour17).css("background-color", "#ff6961");
+    } else {
+      alert('Error');
+    }
+  
+  
+  })
--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# Work_Day_Scheduler
-A simple calendar application that allows a user to save events for each hour of the day by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+# 05 Third-Party APIs: Work Day Scheduler
+
+## The Task
+
+Create a simple calendar application that allows a user to save events for each hour of the day by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+
+You'll need to use a library like [Moment.js](https://momentjs.com/) to work with dates and times. `Moment.js` has historically been the most popular date/time library but is no longer supported by its developers. However, you can still use it for this project, or you can look into one of the following alternatives:
+
+  * [Luxon](https://moment.github.io/luxon/)
+
+  * [Day.js](https://day.js.org/)
+
+  * [date-fns](https://date-fns.org/)
+
+  * [js-Joda](https://js-joda.github.io/js-joda/)
+
+Whichever library you choose, be sure to read the documentation carefully!
+
+## User Story
+
+```
+AS AN employee with a busy schedule
+I WANT to add important events to a daily planner
+SO THAT I can manage my time effectively
+```
+
+## Acceptance Criteria
+
+```
+GIVEN I am using a daily planner to create a schedule:
+
+1. WHEN I open the planner
+    THEN the current day is displayed at the top of the calendar
+2. WHEN I scroll down
+    THEN I am presented with time blocks for standard business hours
+3. WHEN I view the time blocks for that day
+    THEN each time block is color-coded to indicate whether it is in the past, present, or future
+4. WHEN I click into a time block
+    THEN I can enter an event
+5. WHEN I click the save button for that time block
+    THEN the text for that event is saved in local storage
+6. WHEN I refresh the page
+    THEN the saved events persist
+```
+
+## Installation
+
+Deployment URL
+
+https://DONT FORGET ME/index.html
+
+GitHub URL
+
+https://DONT FORGET ME/Third-Party-API-05.git
+
+
+## Mock-Up - Of the result
+
+The following image shows the web application's appearance and functionality:
+
+![Third party APIs (layout)](DONT FORGET ME)
